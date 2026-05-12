4c3a873f8d1b1cd4603e64d23f7045739b5c35ec
Unifafibe <Unifafibe@UNI-LAB2-001>
2026-05-12 20:03:36 -0300

    Adicionar arquivos de projeto.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppMVC")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppMVC.Pages")]
